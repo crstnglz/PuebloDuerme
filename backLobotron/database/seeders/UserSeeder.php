5cb04999fc4952aae3b412bc5bb9e6ff1044f9eb
CarlosRamirezSantos <carlosramii2304@gmail.com>
2025-11-11 10:00:26 +0100

    feat: Añado factoría y seeder para User

---
+++ backLobotron/database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User; 
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //crea 10 usuarios "humanos".
+        User::factory(10)->create();
+
+        //crea 2 usuarios "bot".
+        User::factory(2)->bot()->create();
+    }
+}
