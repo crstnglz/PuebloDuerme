b2565efd12ba0901d72c5a2928a9e57f423b1893
Cristina <cris.glez512@gmail.com>
2025-11-18 22:08:36 +0100

    feat: agrego subida imagen parte back

---
+++ backLobotron/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class ProfileController extends Controller
+{
+    public function uploadImage(Request $request)
+    {
+        //Validación de archivo
+        $messages = [
+            'image.required' => 'Falta el archivo',
+            'image.mimes' => 'Tipo no soportado',
+            'image.max' => 'El archivo excede el tamaño máximo permitido',
+        ];
+
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // 2MB
+        ], $messages);
+
+        if ($validator->fails()) {
+        return response()->json($validator->errors(), 422);
+        }
+
+
+        if($request->hasFile('image') && $request->file('image')->isValid())
+        {
+            try
+            {
+                $file = $request->file('image');
+                //Generamos nombre único para imagen
+                //Obtenemos nombre y extensión por separado
+                $originalName = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+
+                $extension = $file->getClientOriginalExtension();
+
+                //Generamos nombre único y seguro
+                $filename = uniqid('img_') . '_' . Str::slug($originalName) . '.' . $extension;
+
+                //Subimos archivo usando cloudinary
+                $uploadedFilePath = Storage::disk('cloudinary')->putFileAs('laravel', $file, $filename);
+
+                //Obtenemos URL pública
+                $url = Storage::disk('cloudinary')->url($uploadedFilePath);
+
+                return response()->json(['url' => $url], 200);
+            } catch(\Exception $e)
+            {
+                return response()->json(['error' => 'Error al subir la imagen: ' . $e->getMessage()], 500);
+            }
+        }
+        return response()->json(['error' => 'No se recibió ningún archivo.'], 400);
+    }
+}
--- backLobotron/routes/api.php
+++ backLobotron/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/profile/uploadImage', [ProfileController::class, 'uploadImage']);
