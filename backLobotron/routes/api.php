7c443eeb0eab153af844bba4ced29d6d1b885115
CarlosRamirezSantos <carlosramii2304@gmail.com>
2025-11-19 02:44:53 +0100

    feat: añado la gestión CRUD completa de usuarios (solo para admin)

--- backLobotron/routes/api.php
+++ backLobotron/routes/api.php
@@ -3,6 +3,22 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+// CRUD de usuarios
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::post('/users', [UserController::class, 'store']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+Route::patch('/users/{id}/password', [UserController::class, 'updatePassword']);
+
+});
+
