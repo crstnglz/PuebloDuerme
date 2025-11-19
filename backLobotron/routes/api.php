1a16ecfdd5f666ad6bddba6869580e87dd8d7aaa
CarlosRamirezSantos <carlosramii2304@gmail.com>
2025-11-19 03:49:53 +0100

    feat: implemento autorización para CRUD de usuarios
    He utilizado las abilities de los tokens de Laravel Sanctum

--- backLobotron/routes/api.php
+++ backLobotron/routes/api.php
@@ -12,13 +12,18 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
-// CRUD de usuarios
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-Route::post('/users', [UserController::class, 'store']);
-Route::put('/users/{id}', [UserController::class, 'update']);
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
-Route::patch('/users/{id}/password', [UserController::class, 'updatePassword']);
+
+    Route::middleware('abilities:admin')->group(function () {
+        
+        // CRUD de usuarios
+        Route::get('/users', [UserController::class, 'index']);
+        Route::get('/users/{id}', [UserController::class, 'show']);
+        Route::post('/users', [UserController::class, 'store']);
+        Route::put('/users/{id}', [UserController::class, 'update']);
+        Route::delete('/users/{id}', [UserController::class, 'destroy']);
+        Route::patch('/users/{id}/password', [UserController::class, 'updatePassword']);
+    });
+
 
 });
 
