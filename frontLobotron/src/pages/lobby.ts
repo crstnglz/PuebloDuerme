0da6009c0ac862c230cb5e63d2be299e935ebfb6
Cristina <cris.glez512@gmail.com>
2025-11-26 13:27:58 +0100

    feat: añado funcionalidad de crear partida

--- frontLobotron/src/pages/lobby.ts
+++ frontLobotron/src/pages/lobby.ts
@@ -4,6 +4,12 @@ export function initLobby() {
   const lobbyNickname = document.getElementById('lobbyNickname');
   const lobbyProfileImg = document.getElementById('lobbyProfileImg') as HTMLImageElement | null;
 
+  // Elementos del Modal
+  const createGameModal = document.getElementById('createGameModal') as HTMLDivElement | null;
+  const cancelCreateGameBtn = document.getElementById('cancelCreateGameBtn') as HTMLButtonElement | null;
+  const confirmCreateGameBtn = document.getElementById('confirmCreateGameBtn') as HTMLButtonElement | null;
+  const gameNameInput = document.getElementById('gameNameInput') as HTMLInputElement | null;
+ 
   if (userRaw && lobbyNickname) {
     try {
       const user = JSON.parse(userRaw);
@@ -21,12 +27,41 @@ export function initLobby() {
   const joinGameBtn = document.getElementById('joinGameBtn');
   const createGameBtn = document.getElementById('createGameBtn');
 
+  // Unirse a partida #WIP
   joinGameBtn?.addEventListener('click', () => {
     console.log('Unirse a partida (pendiente de implementar)');
   });
 
   createGameBtn?.addEventListener('click', () => {
-    console.log('Crear partida (pendiente de implementar)');
+
+    if(!createGameModal || !gameNameInput) return
+
+    createGameModal.classList.remove("hidden");
+    gameNameInput.value = ""
+    gameNameInput.focus()
   });
 
+  // Cerrar Modal
+  cancelCreateGameBtn?.addEventListener('click', () => {
+    createGameModal?.classList.add('hidden');
+  })
+
+  //Confirmar creación Partida
+  confirmCreateGameBtn?.addEventListener('click', () => {
+    if(!gameNameInput || !createGameModal) return
+
+    const gameName = gameNameInput.value.trim()
+
+    if(gameName.length === 0)
+    {
+      alert("Pon un nombre válido") 
+      return
+    }
+
+    console.log("Crear partida con nombre:", gameName)
+
+  //TODO: post backend
+  // createGameBtn(gameName)
+    createGameModal.classList.add('hidden')
+  })
 }
