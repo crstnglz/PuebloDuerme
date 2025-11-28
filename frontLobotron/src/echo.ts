4512fbada3c7c0a1ce1885cc99296c1481e5bdf5
CarlosRamirezSantos <carlosramii2304@gmail.com>
2025-11-28 23:58:22 +0100

    fix: realizo algunos arreglos de la configuración inicial de los websockets

--- frontLobotron/src/echo.ts
+++ frontLobotron/src/echo.ts
@@ -13,7 +13,7 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
   broadcaster: 'reverb',
   key: import.meta.env.VITE_REVERB_APP_KEY,
-  wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
+  wsHost: import.meta.env.VITE_REVERB_HOST,
   wsPort: Number(import.meta.env.VITE_REVERB_PORT ?? 9090),
   wssPort: Number(import.meta.env.VITE_REVERB_PORT ?? 9090),
   forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
