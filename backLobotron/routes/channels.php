aaff8ba93cbdc15cd73da60f68e5fc1d3c65988c
Cristina <cris.glez512@gmail.com>
2025-12-08 10:52:57 +0100

    fix: arreglo canal game

--- backLobotron/routes/channels.php
+++ backLobotron/routes/channels.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use App\Models\Game;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
@@ -8,6 +9,12 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
 
 Broadcast::channel('game.{gameId}', function ($user, $gameId) {
     if(!$user) return false;
+
+    $game = Game::fin($gameId);
+    if(!$game) return false;
+
+    $isInGame = $game->players()->where('user_id', $user->id)->exists();
+    if (!$isInGame) return false;
 
     return [
         'id' => $user->id,
