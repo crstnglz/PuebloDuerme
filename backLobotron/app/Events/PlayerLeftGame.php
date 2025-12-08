d0cbcc38de561833cd3d8e102761febd369f055f
Cristina <cris.glez512@gmail.com>
2025-12-08 10:48:20 +0100

    feat: añado evento playerleftgame

---
+++ backLobotron/app/Events/PlayerLeftGame.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PlayerLeftGame implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public int $gameId;
+    public int $userId;
+    public string $username;
+    public int $remainingPlayers;
+
+    public function __construct($gameId, $userId, $username, $remainingPlayers)
+    {
+        $this->gameId = $gameId;
+        $this->userId = $userId;
+        $this->username = $username;
+        $this->remainingPlayers = $remainingPlayers;
+    }
+
+    public function broadcastOn()
+    {
+        return new PrivateChannel("game.{$this->gameId}");
+    }
+
+    public function broadcastAs()
+    {
+        return 'player.left';
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'gameId' => $this->gameId,
+            'userId' => $this->userId,
+            'username' => $this->username,
+            'remainingPlayers' => $this->remainingPlayers
+        ];
+    }
+}
