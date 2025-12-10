3f548cb88a751d62fa2420d9c8ac5726cf132fd2
juanxodf <martinpozuelojuan@gmail.com>
2025-12-10 12:47:57 +0100

    refactor: Añado campos a las rutas de canales

--- backLobotron/routes/channels.php
+++ backLobotron/routes/channels.php
@@ -8,7 +8,7 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('game.{gameId}', function () {
+Broadcast::channel('game.{gameId}', function ($user,$gameId) {
     return true;
 });
 
